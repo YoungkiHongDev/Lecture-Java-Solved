8fb795491277d17d17c42846494ed52b6a29eed0
YoungkiHongDev <dudrl1410@naver.com>
2022-11-23 04:46:55 +0900

    String 02. 대소문자 변환

---
+++ String02.java
@@ -0,0 +1,22 @@
+/*
+ * 2. 대소문자 변환
+ */
+import java.util.Scanner;
+
+public class String02 {
+    public static void main(String[] args) {
+        //문자열 입력받기
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        //반복문으로 한글자씩 대소문자 판단 후 대소문자 바꿔주기
+        String answer = "";
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) answer = answer + Character.toLowerCase(c);
+            else answer = answer + Character.toUpperCase(c);
+        }
+
+        System.out.println(answer);
+        sc.close();
+    }
+}
